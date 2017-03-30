d55db7696cb353f99dd7d2fbc03e085597029b31
Doug Woznicki <dwoznicki@msn.com>
2017-03-30 10:20:27 -0400

    Fixed

--- untitled-3-30.ps1
+++ untitled-3-30.ps1
@@ -22,14 +22,14 @@ Process {
             }
             Catch{}
 
-            $curobj = New-Object PSObject -Property @{
+            $curobj = New-Object PSObject -Property ([ordered]@{
              Server = $Server
              "VM Capacity (GB)" = [Math]::round(($disk.Size / $ConvertToGB), 3)
              "VM FreeDisk (GB)" = [Math]::round(($disk.FreeSpace / $ConvertToGB), 3)
              Host = $VMHost.hostname
              "Host Capacity (GB)" = [Math]::round(($hostdisk.Size / $ConvertToGB), 3)
              "Host FreeDisk (GB)" = [Math]::round(($hostdisk.FreeSpace / $ConvertToGB), 3)
-            }
+            }) # | Select-object Server, VM Capacity (GB), VM FreeDisk (GB), Host, Host Capacity (GB), Host FreeDisk (GB) 
              #$Server + "," + [Math]::round(($disk.Size / $ConvertToGB), 3) + "," + [Math]::round(($disk.FreeSpace / $ConvertToGB), 3) + "," + $vmhost.hostname + "," + [Math]::round(($hostdisk.Size / $ConvertToGB), 3) + "," + [Math]::round(($hostdisk.FreeSpace / $ConvertToGB), 3)
         
             $Report += $curobj
