35d99a4aee9856d9aa89725bc67a3cd97433c4af
Doug Woznicki <dwoznicki@msn.com>
2017-06-01 14:38:22 -0400

    Added some  code from running a function remotely

--- scratchpad.ps1
+++ scratchpad.ps1
@@ -3,3 +3,24 @@
 
 
 Get-Process
+
+
+Function Get-NetConfig {
+    [CmdletBinding()]
+    Param (
+        $InterfaceIndex,
+        $AddressFamily
+    )
+
+    $NodeName = $env:ComputerName
+    $NetAdapter = Get-NetAdapter -InterfaceIndex $InterfaceIndex
+    $IPconfig = Get-NetIPAddress -InterfaceIndex $InterfaceIndex -AddressFamily $AddressFamily
+    $DNS = (Get-DnsClientServerAddress -InterfaceIndex $InterfaceIndex).ServerAddresses
+    $obj = [PSCustomObject]@{NodeName = $NodeName; IPAddress = $IPconfig.IPAddress; MacAddress = $NetAdapter.MacAddress; DNS = $DNS}
+    $obj
+}
+
+Invoke-Command -ComputerName ssdsrv108 `
+    -ScriptBlock ${Function:Get-NetConfig} `
+    -ArgumentList '12', 'IPV4' |
+    Select-Object NodeName, IPaddress, MacAddress, DNS
