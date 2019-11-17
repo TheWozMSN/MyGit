95feeb239757cae843731b7fdae9cf2e91903b72
Doug Woznicki <dwoznicki@msn.com>
2019-11-17 16:24:37 -0500

    Starting with Docker

---
+++ Docker/MyDockerImages.ps1
@@ -0,0 +1,5 @@
+docker pull mcr.microsoft.com/windows/servercore:1909
+docker pull mcr.microsoft.com/powershell
+docker pull mcr.microsoft.com/windows:1909
+docker pull mcr.microsoft.com/windows/nanoserver:1909
+docker pull mcr.microsoft.com/windows/servercore/iis:windowsservercore-1909
